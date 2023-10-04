925f7e313ee760f6340e644eece6dc0288199579
Otieno92 <otienofredricko409@gmail.com>
2023-10-04 06:20:31 -0700

    101-remove_char_at.py

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-# Graham S. Paul (101-remove_char_at.py)
-
-def remove_char_at(str, n):
-    """Write a mimiced string without nature at location n"""
-    i = f"{str[:n]:s}{str[(n + 1):]}" \
-        if n >= 0 else str
-     return i
+def remove_chaer_at(str, n):
+    newStr = ""
+    for i in range(len(str)):
+        if i == n:
+            continue
+        else:
+            newStr += str[i]
+            return newStr
