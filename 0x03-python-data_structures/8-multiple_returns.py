a00fd45d109f1627b1494f49f732a7864d2cecf0
Otieno92 <otienofredricko409@gmail.com>
2023-10-08 14:29:31 -0700

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+# 8-multiple_returns.py
+
+
 def multiple_returns(sentence):
-    if(len(sentence) > 0):
-        return((len(sentence), sentence[0]))
-    return((len(sentence), None))
+    """Returns the length of a string and its first character."""
+    if sentence == "":
+        return (0, None)
+    return (len(sentence), sentence[0])
